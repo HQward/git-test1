cd0c7b136cc165c6e44d97c9f51919473d3987fd
HQward <1294105934@qq.com>
2022-11-12 21:14:44 +0800

    first commit

---
+++ git2/src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author HuQinghua email:hqh@shu.edu.cn
+ * @create 2022-11-12 21:09
+ */
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("hello git!");
+	}
+}
